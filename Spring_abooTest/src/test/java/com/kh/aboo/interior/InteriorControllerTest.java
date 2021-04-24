09dff6dfc4484f88125a8028f8ec4bd5b95172ba
minh0380 <minh0380@naver.com>
2021-04-24 14:33:00 +0900

    intcmtmodify test

--- Spring_abooTest/src/test/java/com/kh/aboo/interior/InteriorControllerTest.java
+++ Spring_abooTest/src/test/java/com/kh/aboo/interior/InteriorControllerTest.java
@@ -1,6 +1,7 @@
 package com.kh.aboo.interior;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.Before;
@@ -14,6 +15,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.kh.aboo.board.interior.model.vo.IntCmt;
+
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
@@ -26,6 +29,18 @@ public class InteriorControllerTest {
 	@Before
 	public void setup() {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+	
+	@Test
+	public void intCmtModify() throws Exception {
+		IntCmt intCmt = new IntCmt();
+		intCmt.setIntCmtNo("100020");
+		intCmt.setIntCmtContent("목객체를 이용한 댓글 수정");
+		
+		this.mockMvc.perform(post("/board/interior/intcmtmodify")
+				.param("intCmtNo", intCmt.getIntCmtNo())
+				.param("intCmtContent", intCmt.getIntCmtContent()))
+		.andDo(print());
 	}
 	
 	@Test
